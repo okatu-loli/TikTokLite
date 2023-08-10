707512ae48dd1e921fcafa15e836609e5ed075b1
千石 <1835038031@qq.com>
2023-08-10 08:55:18 +0800

    feat：修改MD5算法为更安全的Bcrypt算法

--- common/tool/encryption.go
+++ common/tool/encryption.go
@@ -1,22 +1,29 @@
 package tool
 
 import (
-	"crypto/md5"
-	"fmt"
-	"io"
+	"golang.org/x/crypto/bcrypt"
 )
 
-// MD5加密
-func Md5ByString(str string) string {
-	m := md5.New()
-	_, err := io.WriteString(m, str)
+// BcryptByString 将字符串加密为bcrypt散列
+func BcryptByString(str string) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword([]byte(str), bcrypt.DefaultCost)
 	if err != nil {
-		panic(err)
+		return "", err
 	}
-	arr := m.Sum(nil)
-	return fmt.Sprintf("%x", arr)
+	return string(hash), nil
 }
 
-func Md5ByBytes(b []byte) string {
-	return fmt.Sprintf("%x", md5.Sum(b))
+// BcryptByBytes 将字节切片加密为bcrypt散列
+func BcryptByBytes(b []byte) (string, error) {
+	hash, err := bcrypt.GenerateFromPassword(b, bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(hash), nil
+}
+
+// CheckPasswordHash 用于比较密码与其散列版本
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
 }
